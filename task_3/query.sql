a3b355c439140f03bfaec1a87b3ec2f5fdedf818
Dmytro Bezvoshchuk <bezvoshchuk@gmail.com>
2025-06-08 11:01:28 +0300

    Task 3

---
+++ task_3/query.sql
@@ -0,0 +1,10 @@
+-- 3. Напишіть SQL команду, за допомогою якої можна обрати унікальні
+-- значення колонок category_id та price таблиці products.
+
+-- Оберіть порядок виведення на екран за спаданням значення price та виберіть тільки 10 рядків.
+-- Перевірте правильність виконання команди в MySQL Workbench.
+
+SELECT DISTINCT category_id, price
+FROM mydb.products
+ORDER BY price DESC
+    LIMIT 10;
